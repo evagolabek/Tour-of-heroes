8e4cf64130b2c0c7ba2f261cbf7a82a8ae1f5f8a
evagolabek <golabek.eva@gmail.com>
2018-10-22 14:10:11 +0200

    select heroes and styling add

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,8 +8,13 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
   
+  heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
+
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
